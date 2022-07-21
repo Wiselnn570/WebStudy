888587f88613d3afe7a16eff301e96d28c1c10cb
Wiselnn <Wiselnn_570@163.com>
2022-07-21 20:46:56 +0800

    hello world

---
+++ my-app/react-app/src/component/app.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <React.Fragment>
+                
+                {/* <NavBar /> */}
+                <div className="container"><h1>Hello world</h1></div>
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default App;
